11a9ab9e09eccdbfafd077602184abf774f781ea
asia-jankowska00 <asia.jankowska00@gmail.com>
2021-05-28 12:08:25 +0200

    chore: fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,48 @@
 module.exports = {
-  extends: 'galex',
-  plugins: ['prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'simple-import-sort', 'prettier'],
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
   rules: {
     'new-cap': 'off',
-    'prettier/prettier': 'error',
-    'linebreak-style': 0,
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'react/display-name': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
   },
 }
