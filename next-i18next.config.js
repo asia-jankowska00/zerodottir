ad808a1406801fa90bd75dd6e2b1da1982e7f624
asia-jankowska00 <asia.jankowska00@gmail.com>
2021-06-05 20:57:43 +0200

    fix: i18n path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,11 @@
 // const { initReactI18next } = require('react-i18next')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
 
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'dk'],
   },
+  localePath: path.resolve('./public/locales'),
 }
